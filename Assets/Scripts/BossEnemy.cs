816220ec0ac52d521e3df7421e2795683bb3f6af
Rajat1380 <rajat_ku@nid.edu>
2019-08-07 17:57:33 +0530

    BossEnemy

---
+++ Assets/Scripts/BossEnemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemy : MonoBehaviour
+
+{
+
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistacne;
+    public Transform player;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public GameObject rightprojectile;
+    public GameObject leftprojectile;
+    public Transform Rightprojectile;
+    public Transform Leftprojectile;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        timeBtwShots = startTimeBtwShots;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        Rightprojectile = GameObject.FindGameObjectWithTag("Right").transform;
+
+        Leftprojectile = GameObject.FindGameObjectWithTag("Left").transform;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+        }
+        else if( Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistacne)
+        {
+            transform.position = this.transform.position;
+        }
+        else if( Vector2.Distance( transform.position, player.position) < retreatDistacne)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+        if(timeBtwShots <= 0)
+        {
+            Instantiate(rightprojectile,Rightprojectile.position, Quaternion.identity);
+            Instantiate(leftprojectile,Leftprojectile.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
+ 
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+
+    private Transform Player;
+    private Vector2 target;
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform ;
+
+        target = new Vector2( Player.position.x, Player.position.y);
+
+    }
+
+    void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, Player.position, speed * Time.deltaTime);
+
+        if( transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyProjectile();
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
